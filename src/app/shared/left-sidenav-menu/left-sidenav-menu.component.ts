622f2d20dd88760d2e2ceec5866a5b442d48766c
tihandjan <tihandjan198921@gmail.com>
2017-03-11 07:02:06 +0300

    basic navigation structure

---
+++ src/app/shared/left-sidenav-menu/left-sidenav-menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-left-sidenav-menu',
+  templateUrl: './left-sidenav-menu.component.html',
+  styleUrls: ['./left-sidenav-menu.component.scss']
+})
+export class LeftSidenavMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/shared/right-sidenav-menu/right-sidenav-menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-right-sidenav-menu',
+  templateUrl: './right-sidenav-menu.component.html',
+  styleUrls: ['./right-sidenav-menu.component.scss']
+})
+export class RightSidenavMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
