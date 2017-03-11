04064aa1224f9181e3ec1ea648af66fbbaaad887
tihandjan <tihandjan198921@gmail.com>
2017-03-12 01:18:49 +0300

    right side nav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainComponent } from './main/main.component';
 
 const routes: Routes = [
   {
-    path: '',
-    children: []
+    path: '', component: MainComponent
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,20 @@ import { HttpModule } from '@angular/http';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { LeftSidenavMenuComponent } from './shared/left-sidenav-menu/left-sidenav-menu.component';
+import { RightSidenavMenuComponent } from './shared/right-sidenav-menu/right-sidenav-menu.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    LeftSidenavMenuComponent,
+    RightSidenavMenuComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
