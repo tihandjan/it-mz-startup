2273f3dec726be6212cf1cc12e48944e38fc3f07
tihandjan <tihandjan198921@gmail.com>
2017-03-12 04:32:39 +0300

    add animation

--- src/app/shared/right-sidenav-menu/right-sidenav-menu.component.ts
+++ src/app/shared/right-sidenav-menu/right-sidenav-menu.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit} from '@angular/core';
+import {
+   Component, 
+   OnInit,
+   trigger,
+   state,
+   animate,
+   style,
+   transition
+} from '@angular/core';
 
 @Component({
   selector: 'app-right-sidenav-menu',
   templateUrl: './right-sidenav-menu.component.html',
-  styleUrls: ['./right-sidenav-menu.component.scss']
+  styleUrls: ['./right-sidenav-menu.component.scss'],
+  animations: [
+    trigger('elementState', [
+      state('inactive', style({
+        right: '-280px'
+      })),
+      state('active', style({
+        right: 0
+      })),
+      transition('inactive <=> active', animate('300ms ease-out'))
+    ])
+  ]
 })
 export class RightSidenavMenuComponent implements OnInit {
-
+  state: string = 'inactive';
   constructor() { }
 
   ngOnInit() {
+  }
+
+  toggleMenu() {
+    this.state = this.state === 'inactive' ? 'active' : 'inactive';
   }
 
 
