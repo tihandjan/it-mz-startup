69b4a4da56d1bfbfdaf570743f6b5334e6a97167
tihandjan <tihandjan198921@gmail.com>
2017-03-26 06:15:06 +0300

    left side menu mobile optimization

--- src/app/shared/left-sidenav-menu/left-sidenav-menu.component.ts
+++ src/app/shared/left-sidenav-menu/left-sidenav-menu.component.ts
@@ -1,19 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import {
+   Component, OnInit, 
+   style,
+   state,
+   trigger,
+   animate,
+   transition
+} from '@angular/core';
 
 @Component({
   selector: 'app-left-sidenav-menu',
   templateUrl: './left-sidenav-menu.component.html',
-  styleUrls: ['./left-sidenav-menu.component.scss']
+  styleUrls: ['./left-sidenav-menu.component.scss'],
+  animations: [
+    trigger('elementState', [
+      state('inactive', style({
+        left: '-190px'
+      })),
+      state('active', style({
+        left: '0'
+      })),
+      transition('active <=> inactive', animate('300ms ease-out'))
+    ])
+  ]
 })
 export class LeftSidenavMenuComponent implements OnInit {
-
+  state: string = 'inactive';
   constructor() { }
 
   ngOnInit() {
   }
 
-  open($event) {
-    console.log($event)
+  openMenu() {
+    this.state = this.state == 'inactive' ? 'active' : 'inactive';
   }
 
 }
